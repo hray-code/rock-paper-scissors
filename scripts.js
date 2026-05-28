a365ed1ca6bb1825dfb8e574fc42242e9b30992d
Haley Raymond <270042230+hray-code@users.noreply.github.com>
2026-05-27 19:12:47 -0500

    Added logic to playGame function that determines winner, gets new choices, and loops through to complete 5 rounds. Needed to store scores as global variables.

--- scripts.js
+++ scripts.js
@@ -1,61 +1,98 @@
-//------------------------------------ V2: ROCK PAPER SCISSORS ------------------------------------- //
+//---------------------------------------- ROCK PAPER SCISSORS --------------------------------------------//
 
+//get computer choice
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
+    let result = "";
         if(randomNumber === 0)
-            return "rock";
+            result = "rock";
         else if(randomNumber === 1)
-            return "paper";
+            result = "paper";
         else if(randomNumber === 2)
-            return "scissors";
-    }
+            result = "scissors";
+        return result;
+ }
 
+ //get human choice
 function getHumanChoice(){
     let humanInput = prompt("Enter rock, paper, or scissors.");
     let result = humanInput.toLowerCase();
     return result;
 }
 
-for(let i = 0; i < 5; i++){
-    startGame();
-}
+//store human and computer choices
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
 
-function startGame(){
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    playGame(computerChoice, humanChoice);
-}
+//keep track of how many rounds have been played
+let roundsPlayed = 5;
+
+//keep score
+let computerScore = 0;
+let humanScore = 0;
+
+//start game and pass in computer and human choices
+playGame(computerChoice, humanChoice);
 
 function playGame(computerChoice, humanChoice){
 
-    let computerScore = 0; 
-    let humanScore = 0; 
+    //let gameMessage = `Computer Score: ${computerScore} | User Score: ${humanScore}`;
 
         if(computerChoice === "rock" && humanChoice === "scissors"){
             computerScore++;
-            console.log("Rock beats scissors, computer wins this round.")
+            roundsPlayed--;
+            console.log("Rock beats scissors, computer wins this round.");
+            console.log(`Computer pick: ${computerChoice} | User choice: ${humanChoice}`);
         }
         else if(computerChoice === "paper" && humanChoice === "rock"){
             computerScore++;
-            console.log("Paper beats rock, computer wins this round.")
+            roundsPlayed--;
+            console.log("Paper beats rock, computer wins this round.");
+            console.log(`Computer pick: ${computerChoice} | User choice: ${humanChoice}`)
         }
         else if(computerChoice === "scissors" && humanChoice === "paper"){
             computerScore++;
-            console.log("Scissors beats paper, computer wins this round.")
+            roundsPlayed--;
+            console.log("Scissors beats paper, computer wins this round.");
+            console.log(`Computer pick: ${computerChoice} | User choice: ${humanChoice}`)
         }
         else if(computerChoice === "scissors" && humanChoice === "rock"){
             humanScore++;
-            console.log("Rock beats scissors, user wins this round.")
+            roundsPlayed--;
+            console.log("Rock beats scissors, user wins this round.");
+            console.log(`Computer pick: ${computerChoice} | User choice: ${humanChoice}`)
         }
         else if(computerChoice === "paper" && humanChoice === "scissors"){
             humanScore++;
-            console.log("Scissors beats paper, user wins this round.")
+            roundsPlayed--;
+            console.log("Scissors beats paper, user wins this round.");
+            console.log(`Computer pick: ${computerChoice} | User choice: ${humanChoice}`)
         }
         else if(computerChoice === "rock" && humanChoice === "paper"){
             humanScore++;
-            console.log("Paper beats rock, user wins this round.")
+            roundsPlayed--;
+            console.log("Paper beats rock, user wins this round.");
+            console.log(`Computer pick: ${computerChoice} | User choice: ${humanChoice}`)
         }
         else{
             console.log("Tie.");
-        } 
+            roundsPlayed--;
+            console.log(`Computer pick: ${computerChoice} | User choice: ${humanChoice}`)
+        }
+        
+        if(roundsPlayed === 0){
+            if(computerScore > humanScore){
+                console.log("Computer Wins!");
+            }else if(humanScore > computerScore){
+                console.log("User Wins!");
+            }else if(computerScore === humanScore){
+                console.log("No winner.");
+            }
+        }
+
+        if(roundsPlayed < 5 && roundsPlayed > 0){ //loop through 5 times
+            let getNewComputerChoice = getComputerChoice(); //get new choice
+            let getNewHumanChoice = getHumanChoice(); //get new choice
+            playGame(getNewComputerChoice, getNewHumanChoice); //play again with new choices
+        }
 }
